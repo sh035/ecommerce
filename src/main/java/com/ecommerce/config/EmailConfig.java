858cb1da5113bb7eb4931a53e534784775495cf3
sh035 <saab35@naver.com>
2024-07-22 18:26:17 +0900

    refactor : 이메일 서버 설정을 application-email.yml 으로 이동

--- src/main/java/com/ecommerce/config/EmailConfig.java
+++ src/main/java/com/ecommerce/config/EmailConfig.java
@@ -1,6 +1,7 @@
 package com.ecommerce.config;
 
 import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -8,15 +9,27 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class EmailConfig {
+
+  @Value("${spring.email.host}")
+  private String host;
+
+  @Value("${spring.email.port}")
+  private int port;
+
+  @Value("${spring.email.username}")
+  private String username;
+
+  @Value("${spring.email.password}")
+  private String password;
+
   @Bean
   public JavaMailSender mailSender() {
-
     JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-    mailSender.setHost("smtp.gmail.com");
-    mailSender.setPort(587);
-    mailSender.setUsername("2sh9735@gmail.com");
-    mailSender.setPassword("bleydsystkepqohb"); // 구글 앱 비밀번호
+    mailSender.setHost(host);
+    mailSender.setPort(port);
+    mailSender.setUsername(username);
+    mailSender.setPassword(password); // 구글 앱 비밀번호
 
     Properties javaMailProperties = new Properties();
     javaMailProperties.put("mail.transport.protocol", "smtp");
