ba3af4829c4afc0bccfc8f4e9b10d0b537901590
sh035 <saab35@naver.com>
2024-09-10 19:03:51 +0900

    feat: 프로퍼티 환경변수로 변경

--- src/main/java/com/ecommerce/config/EmailConfig.java
+++ src/main/java/com/ecommerce/config/EmailConfig.java
@@ -1,6 +1,7 @@
 package com.ecommerce.config;
 
 import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -8,6 +9,19 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class EmailConfig {
+
+  @Value("${spring.email.host}")
+  private String host;
+
+  @Value("${spring.email.port}")
+  private int port;
+
+  @Value("${spring.email.username}")
+  private String username;
+
+  @Value("${spring.email.password}")
+  private String password;
+
   @Bean
   public JavaMailSender mailSender() {
 
