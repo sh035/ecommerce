82cfb44c024d23978990cf39efb8c9cde2b0c315
sh035 <saab35@naver.com>
2024-09-21 14:09:59 +0900

    refactor: 상품 유효성 검사 변경

--- src/main/java/com/ecommerce/product/domain/dto/ProductCreateDto.java
+++ src/main/java/com/ecommerce/product/domain/dto/ProductCreateDto.java
@@ -1,6 +1,8 @@
 package com.ecommerce.product.domain.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
@@ -19,25 +21,24 @@ public class ProductCreateDto {
     @Size(max = 100, message = "상품 이름은 100자까지 입력할 수 있습니다.")
     private String name;
 
-    @NotBlank(message = "가격을 입력해주세요.")
-    @PositiveOrZero(message = "가격은 음수를 입력할 수 없습니다.")
+    @PositiveOrZero(message = "올바른 값을 입력해주세요.")
     private int price;
 
     @NotBlank(message = "상품 설명을 입력해주세요.")
     @Size(max = 3000, message = "상품 설명은 3000자까지 입력할 수 있습니다.")
     private String description;
 
-    @NotBlank(message = "카테고리를 선택해주세요.")
+    @NotNull(message = "카테고리를 선택해주세요.")
     private long parentCategoryId;
 
-    @NotBlank(message = "카테고리를 선택해주세요.")
+    @NotNull(message = "카테고리를 선택해주세요.")
     private long childCategoryId;
 
-    @NotBlank(message = "배송비를 입력해주세요.")
-    @PositiveOrZero(message = "0 이상 입력해주세요.")
+    @NotNull(message = "배송비를 입력해주세요.")
+    @PositiveOrZero(message = "올바른 값을 입력해주세요.")
     private int deliveryCharge;
 
-    @NotBlank(message = "수량을 입력해주세요.")
-    @Size(min = 1, message = "1개 이상 입력해주세요.")
+    @NotNull(message = "수량을 입력해주세요.")
+    @Min(value = 1, message = "1개 이상 입력해주세요.")
     private int qty;
 }
--- src/main/java/com/ecommerce/product/domain/dto/ProductUpdateDto.java
+++ src/main/java/com/ecommerce/product/domain/dto/ProductUpdateDto.java
@@ -1,7 +1,9 @@
 package com.ecommerce.product.domain.dto;
 
 import com.ecommerce.product.domain.enums.ProductStatus;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import java.util.List;
@@ -21,26 +23,25 @@ public class ProductUpdateDto {
     @Size(max = 100, message = "상품 이름은 100자까지 입력할 수 있습니다.")
     private String name;
 
-    @NotBlank(message = "가격을 입력해주세요.")
-    @PositiveOrZero(message = "가격은 음수를 입력할 수 없습니다.")
+    @PositiveOrZero(message = "올바른 값을 입력해주세요.")
     private int price;
 
     @NotBlank(message = "상품 설명을 입력해주세요.")
     @Size(max = 3000, message = "상품 설명은 3000자까지 입력할 수 있습니다.")
     private String description;
 
-    @NotBlank(message = "카테고리를 선택해주세요.")
+    @NotNull(message = "카테고리를 선택해주세요.")
     private long parentCategoryId;
 
-    @NotBlank(message = "카테고리를 선택해주세요.")
+    @NotNull(message = "카테고리를 선택해주세요.")
     private long childCategoryId;
 
-    @NotBlank(message = "배송비를 입력해주세요.")
-    @PositiveOrZero(message = "0 이상 입력해주세요.")
+    @NotNull(message = "배송비를 입력해주세요.")
+    @PositiveOrZero(message = "올바른 값을 입력해주세요.")
     private int deliveryCharge;
 
-    @NotBlank(message = "수량을 입력해주세요.")
-    @Size(min = 1, message = "1개 이상 입력해주세요.")
+    @NotNull(message = "수량을 입력해주세요.")
+    @Min(value = 1, message = "1개 이상 입력해주세요.")
     private int qty;
 
     private ProductStatus productStatus;
